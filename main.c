0bb98bf1c455b4981ab6516688c9485c0776cc5f
AaritPanda-git <cs25b001@smail.iitm.ac.in>
2026-04-17 09:44:12 +0530

    Added the word suggestion functionality in main.c

--- main.c
+++ main.c
@@ -40,26 +40,31 @@ int main(){
     box(bg, 0, 0);    
     wrefresh(bg);
 
-    WINDOW *search_win;
+    WINDOW *search_win = NULL;
+    WINDOW *suggest_win = NULL;
     char buff[50] = {0};
     char buff_m1[50] = {0};
     char buff_m2[50] = {0};
     char buff_m3[50] = {0};
+    char buff_m5[50] = {0};
+
     int current_mode=4;
-
-    int selected_index = 0;
-    int scroll_offset = 0;
-
-    // int ln_m3 = 3;
-    // int ln_search_m3 = 3;
 
     int rno = 4;
     int cno = 1;
+
+    int srno=4;
+    int scno=1;
 
     while(1){
 
         if(current_mode == 3){
     
+            if(suggest_win != NULL){
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+            }
+
             mvwprintw(bg , 1 , 1 , "Search Mode\n");
             box(bg, 0, 0);
             box(search_win , 0 , 0);
@@ -70,6 +75,14 @@ int main(){
         }
         else if(current_mode == 1){
 
+            if(search_win!=NULL){
+                delwin(search_win);
+                search_win=NULL;
+            }
+            if(suggest_win != NULL){
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+            }
             mvwprintw(bg , 1 , 1 , "Insert Mode\n");
             box(bg, 0, 0);
             wrefresh(bg);
@@ -77,6 +90,14 @@ int main(){
         }
         else if(current_mode == 2){
 
+            if(search_win!=NULL){
+                delwin(search_win);
+                search_win=NULL;
+            }
+            if(suggest_win != NULL){
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+            }
             mvwprintw(bg , 1 , 1 , "Delete Mode\n");
             box(bg, 0, 0);
             wrefresh(bg);
@@ -84,6 +105,15 @@ int main(){
         }
         else if(current_mode == 4){
             
+            if(search_win!=NULL){
+                delwin(search_win);
+                search_win=NULL;
+            }
+            if(suggest_win != NULL){
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+            }
+
             int h, w;
             getmaxyx(bg, h, w);
 
@@ -95,7 +125,7 @@ int main(){
             mvwprintw(bg, h * 0.2, w * 0.1, "Word Count:   %d", wordcount(root));
             mvwprintw(bg, h * 0.3, w * 0.1, "Max Word Length:    %d", max_depth(root)-1);
             mvwprintw(bg, h * 0.4, w * 0.1, "Data Occupied By Nodes:   %.3f MB", (float)node_count(root)*(float)sizeof(TrieNode)/(1024.0f * 1024.0f));
-            mvwprintw(bg, h * 0.5, w * 0.1, "Alpabetical Distribution:");
+            mvwprintw(bg, h * 0.5, w * 0.1, "Alphabetical Distribution:");
             int distri[26];
             alpha_distri(root, distri);
             for (int i=0; i<26; i++) {
@@ -109,22 +139,39 @@ int main(){
             box(bg, 0, 0);
             wrefresh(bg);
 
+            
+
         }
-        int ch = getch();
-        if(ch == KEY_RESIZE){
-            delwin(bg);
-            getmaxyx(stdscr, max_y, max_x);
-            bg = newwin((95*max_y)/100, (99*max_x)/100,0,0);
+        else if(current_mode == 5){
+
+            if(search_win!=NULL){
+                delwin(search_win);
+                search_win=NULL;
+            }
+
+            mvwprintw(bg , 1 , 1 , "Suggestion Mode\n");
             box(bg, 0, 0);
             wrefresh(bg);
+ 
+            box(suggest_win , 0 , 0);
+            wrefresh(bg);
+            touchwin(suggest_win);
+            wrefresh(suggest_win);
+            
         }
-        else if(ch == KEY_F(1)){
+        int ch = getch();
+        
+        if(ch == KEY_F(1)){
             // inserting a new word to the trie
             current_mode = 1;
             
             if(search_win!=NULL){
                 delwin(search_win);
                 search_win=NULL;
+            }
+            if(suggest_win != NULL){
+                delwin(suggest_win);
+                suggest_win = NULL;
             }
             werase(bg);
             box(bg, 0, 0);
@@ -143,6 +190,10 @@ int main(){
                 delwin(search_win);
                 search_win=NULL;
             }
+            if(suggest_win != NULL){
+                delwin(suggest_win);
+                suggest_win = NULL;
+            }
             werase(bg);
             box(bg, 0, 0);
             wrefresh(bg);
@@ -156,6 +207,10 @@ int main(){
         }
         else if(ch == KEY_F(3)){
             //searching
+            if(suggest_win != NULL){
+                delwin(suggest_win);
+                suggest_win = NULL;
+            }
             buff_m3[0]='\0';
             current_mode = 3;
 
@@ -188,9 +243,46 @@ int main(){
                 delwin(search_win);
                 search_win=NULL;
             }
+            if(suggest_win != NULL){
+                delwin(suggest_win);
+                suggest_win = NULL;
+            }
             werase(bg);
             box(bg, 0, 0);
-            wrefresh(bg);      
+            wrefresh(bg);  
+            
+            srno = 4;
+            scno = 1;
+        }
+        else if(ch == KEY_F(5)){
+            current_mode = 5;
+
+            if(search_win!=NULL){
+                delwin(search_win);
+                search_win=NULL;
+            }
+
+            werase(bg);
+            box(bg, 0, 0);
+            wrefresh(bg);
+            suggest_win = newwin(20, (max_x*39)/100, 3, (max_x*60)/100);
+            touchwin(suggest_win);
+            wrefresh(suggest_win);
+
+            mvwprintw(bg , 3 , 1 , "Typing in Correct Word Suggestion Mode...\n");
+            box(bg, 0, 0);
+            wrefresh(bg); 
+            touchwin(suggest_win);
+            wrefresh(suggest_win); 
+            
+            mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+            box(suggest_win , 0 , 0);
+            touchwin(suggest_win);
+            wrefresh(suggest_win);
+            
+            rno = 4;
+            cno = 1;
+
         }
         else if(ch >= 'a' && ch <= 'z'){
             if(current_mode == 1){
@@ -212,7 +304,7 @@ int main(){
                 box(bg, 0, 0);
                 wrefresh(bg);
             }
-            if(current_mode == 3){
+            else if(current_mode == 3){
                 int temp_sz = strlen(buff_m3);
                 buff_m3[temp_sz] = (char)ch;
                 buff_m3[temp_sz + 1] = '\0';
@@ -301,6 +393,103 @@ int main(){
                 vec_free(&results);
 
             }
+            else if(current_mode == 5){
+
+                int temp_sz = strlen(buff_m5);
+                buff_m5[temp_sz] = (char)ch;
+                buff_m5[temp_sz + 1] = '\0';
+
+                mvwprintw(bg , 3 , 1 , "Typing in Correct Word Suggestion Mode...\n");
+                box(bg, 0, 0);
+                wrefresh(bg);
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+
+                if(scno + strlen(buff_m5) >= (60*max_x)/100){
+                    //clearing out the text at old location
+                    for(int ii = 0 ; ii<strlen(buff_m5)-1 ; ii++){
+                        mvwprintw(bg , srno , scno , " ");
+                        scno++;
+                        box(bg, 0, 0);
+                        wrefresh(bg);
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+                    }
+                    srno++;
+                    scno = 1;
+                    mvwprintw(bg , srno , scno , "%s\n" , buff_m5);
+                    box(bg, 0, 0);
+                    wrefresh(bg);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+                else{
+                    mvwprintw(bg , srno , scno , "%s\n" , buff_m5);
+                    box(bg, 0, 0);
+                    wrefresh(bg);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+
+                //vector* word_correction(TrieNode* root, char* word, int dist);
+                if(search(root , buff_m5) == 1){
+                    werase(suggest_win);
+                    mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                    box(suggest_win , 0 , 0);
+
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+
+                    mvwprintw(suggest_win,2,1, "Exact Word Found\n");
+                    box(suggest_win, 0, 0);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+                else{
+                    if(strlen(buff_m5) <= 3){
+                        vector *results = word_correction(root , buff_m5 , 1);
+                        int n = vec_size(results); 
+
+                        werase(suggest_win);
+                        mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                        box(suggest_win , 0 , 0);
+
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+
+                        for(int i=0;i<n && i<15 ; i++){
+                            mvwprintw(suggest_win , 2+i , 1 , vec_at(results , i));
+                            box(suggest_win, 0, 0);
+                            touchwin(suggest_win);
+                            wrefresh(suggest_win);
+                        }
+
+                        vec_free(&results);
+                    }
+                    else{
+                        vector *results = word_correction(root , buff_m5 , 2);
+                        int n = vec_size(results); 
+
+                        werase(suggest_win);
+                        mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                        box(suggest_win , 0 , 0);
+
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+
+                        for(int i=0;i<n && i<15 ; i++){
+                            mvwprintw(suggest_win , 2+i , 1 , vec_at(results , i));
+                            box(suggest_win, 0, 0);
+                            touchwin(suggest_win);
+                            wrefresh(suggest_win);
+                        }
+
+                        vec_free(&results);
+
+                    }
+                }
+
+            }
         }
         else if(ch == '\n' || ch == '\r' || ch == KEY_ENTER){
             if(current_mode == 1){
@@ -326,27 +515,17 @@ int main(){
                 refresh();
             }
             else{
-                werase(search_win);
-                box(search_win, 0, 0);
-                touchwin(search_win);
-                wrefresh(search_win);
-
+                
                 if(current_mode == 3){
+
+                    werase(search_win);
+                    box(search_win, 0, 0);
+                    touchwin(search_win);
+                    wrefresh(search_win);
+
                     cno += strlen(buff_m3);
                     buff_m3[0] = '\0';
-                    if(cno != 0){
-                        mvwprintw(bg , rno , cno , "%c" , ch);
-                        wrefresh(bg);
-                        touchwin(search_win);
-                        wrefresh(search_win);
-                    }
-                    else{
-                        mvwprintw(bg , rno , 0 , "%c" , ch);
-                        wrefresh(bg);
-                        touchwin(search_win);
-                        wrefresh(search_win);
-                    }
-
+                    
                     if(ch == '\n'){
                         rno++;
                         cno = 1;
@@ -358,6 +537,10 @@ int main(){
                         cno++;
                     }
                     else{
+                        if(cno >= (60*max_x)/100){
+                            rno++;
+                            cno=1;
+                        }
                         mvwprintw(bg , rno , cno , "%c" , ch);
                         cno++;
                     }
@@ -365,6 +548,36 @@ int main(){
                     wrefresh(bg);
                     touchwin(search_win);
                     wrefresh(search_win);
+                }
+                else if(current_mode == 5){
+                    
+                    scno += strlen(buff_m5);
+                    buff_m5[0]='\0';
+                    
+                    if(ch == '\n'){
+                        srno++;
+                        scno = 1;
+                    }
+                    else if(ch == '\t'){
+                        scno += 4;
+                    }
+                    else if(ch == ' '){
+                        scno++;
+                    }
+                    else{
+                        if(scno >= (60*max_x)/100){
+                            srno++;
+                            scno=1;
+                        }
+                        mvwprintw(bg , srno , scno , "%c" , ch);
+                        scno++;
+                    }
+                    
+                    box(bg , 0 , 0);
+                    wrefresh(bg);
+                    touchwin(search_win);
+                    wrefresh(search_win);
+
                 }
             }
         }
@@ -471,6 +684,101 @@ int main(){
                 vec_free(&results);
 
             }
+            else if(current_mode == 5){
+                int temp_sz = strlen(buff_m5);
+                buff_m5[temp_sz-1] = '\0';
+
+                mvwprintw(bg , 3 , 1 , "Typing in Correct Word Suggestion Mode...\n");
+                box(bg, 0, 0);
+                wrefresh(bg);
+                touchwin(suggest_win);
+                wrefresh(suggest_win);
+
+                if(scno + strlen(buff_m5) >= (60*max_x)/100){
+                    //clearing out the text at old location
+                    for(int ii = 0 ; ii<strlen(buff_m5)-1 ; ii++){
+                        mvwprintw(bg , srno , scno , " ");
+                        scno++;
+                        box(bg, 0, 0);
+                        wrefresh(bg);
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+                    }
+                    srno++;
+                    scno = 1;
+                    mvwprintw(bg , srno , scno , "%s\n" , buff_m5);
+                    box(bg, 0, 0);
+                    wrefresh(bg);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+                else{
+                    mvwprintw(bg , srno , scno , "%s\n" , buff_m5);
+                    box(bg, 0, 0);
+                    wrefresh(bg);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+
+                //vector* word_correction(TrieNode* root, char* word, int dist);
+                if(search(root , buff_m5) == 1){
+                    werase(suggest_win);
+                    mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                    box(suggest_win , 0 , 0);
+
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+
+                    mvwprintw(suggest_win,2,1, "Exact Word Found\n");
+                    box(suggest_win, 0, 0);
+                    touchwin(suggest_win);
+                    wrefresh(suggest_win);
+                }
+                else{
+                    if(strlen(buff_m5) <= 3){
+                        vector *results = word_correction(root , buff_m5 , 1);
+                        int n = vec_size(results); 
+
+                        werase(suggest_win);
+                        mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                        box(suggest_win , 0 , 0);
+
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+
+                        for(int i=0;i<n && i<15 ; i++){
+                            mvwprintw(suggest_win , 2+i , 1 , vec_at(results , i));
+                            box(suggest_win, 0, 0);
+                            touchwin(suggest_win);
+                            wrefresh(suggest_win);
+                        }
+
+                        vec_free(&results);
+                    }
+                    else{
+                        vector *results = word_correction(root , buff_m5 , 2);
+                        int n = vec_size(results); 
+
+                        werase(suggest_win);
+                        mvwprintw(suggest_win , 1 , 1 , "Suggestions:\n");
+                        box(suggest_win , 0 , 0);
+
+                        touchwin(suggest_win);
+                        wrefresh(suggest_win);
+
+                        for(int i=0;i<n && i<15 ; i++){
+                            mvwprintw(suggest_win , 2+i , 1 , vec_at(results , i));
+                            box(suggest_win, 0, 0);
+                            touchwin(suggest_win);
+                            wrefresh(suggest_win);
+                        }
+
+                        vec_free(&results);
+
+                    }
+                }
+
+            }
         }
         else if(ch == 'Y'){
             if(current_mode == 2){
@@ -531,28 +839,18 @@ int main(){
                 buff_m2[0] = '\0';
             }
         }
-        else if(ch == '\n' || ch == '\t' || ch == ' ' || ch == '\'' || ch == '\"' || ch == ',' || ch == '.' || ch == ';'){
+        else if(ch == '\n' || ch == '\t' || ch == ' ' || ch == '\'' || ch == '\"' || ch == ',' || ch == '.' || ch == ';' || ch == '!'){
             
-            werase(search_win);
-            box(search_win, 0, 0);
-            touchwin(search_win);
-            wrefresh(search_win);
-
             if(current_mode == 3){
+
+                werase(search_win);
+                box(search_win, 0, 0);
+                touchwin(search_win);
+                wrefresh(search_win);
+
                 cno += strlen(buff_m3);
                 buff_m3[0] = '\0';
-                if(cno != 0){
-                    mvwprintw(bg , rno , cno , "%c" , ch);
-                    wrefresh(bg);
-                    touchwin(search_win);
-                    wrefresh(search_win);
-                }
-                else{
-                    mvwprintw(bg , rno , 0 , "%c" , ch);
-                    wrefresh(bg);
-                    touchwin(search_win);
-                    wrefresh(search_win);
-                }
+                
 
                 if(ch == '\n'){
                     rno++;
@@ -565,6 +863,10 @@ int main(){
                     cno++;
                 }
                 else{
+                    if(cno >= (60*max_x)/100){
+                        rno++;
+                        cno=1;
+                    }
                     mvwprintw(bg , rno , cno , "%c" , ch);
                     cno++;
                 }
@@ -573,12 +875,40 @@ int main(){
                 touchwin(search_win);
                 wrefresh(search_win);
             }
+            else if(current_mode == 5){
+                
+                scno += strlen(buff_m5);
+                buff_m5[0]='\0';
+                
+                if(ch == '\n'){
+                    srno++;
+                    scno = 1;
+                }
+                else if(ch == '\t'){
+                    scno += 4;
+                }
+                else if(ch == ' '){
+                    scno++;
+                }
+                else{
+                    if(scno >= (60*max_x)/100){
+                        srno++;
+                        scno=1;
+                    }
+                    mvwprintw(bg , srno , scno , "%c" , ch);
+                    scno++;
+                }
+                
+                box(bg , 0 , 0);
+                wrefresh(bg);
+                touchwin(search_win);
+                wrefresh(search_win);
+
+            }
 
         }
                
-    }
-
-    
+    }   
 
     endwin();
 }
